0efc060dcc4c3af6736f93d3cba9c4f96c163c63
EthanC43850 <70659338+EthanC43850@users.noreply.github.com>
2023-10-25 15:35:14 -0400

    Front end login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LoginPage from './pages/LoginPage';
+import SignUpPage from './pages/SignUpPage'; 
+import HomePage from './pages/HomePage'; 
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<LoginPage />} />
+        <Route path="/SignUp" element={<SignUpPage />} />
+        <Route path="/HomePage" element={<HomePage />} />
+
+      </Routes>
+    </BrowserRouter>
   );
 }
-
 export default App;
---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,16 @@
+import { useState } from 'react';
+
+function HomePage() {
+
+    return (
+        <section className="vh-20">
+            <div className="container-fluid h-100">
+                <h1>WELCOME TO HOMEPAGE</h1>
+
+            </div>
+        </section>
+    );
+}
+
+
+export default HomePage; 
