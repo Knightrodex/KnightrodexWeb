a08d070640f4c6902c81e304b6a7ea076b462fae
EthanC43850 <70659338+EthanC43850@users.noreply.github.com>
2023-11-13 17:07:44 -0500

    Forgot password page

---
+++ frontend/src/pages/ResetPasswordPage.js
@@ -0,0 +1,27 @@
+import React, { useContext, useState } from "react";
+import Navbar from "../components/Navbar";
+import SearchBar from '../components/SearchBar';
+import Activities from "../components/Activities";
+import { UserContext } from '../UserContext';
+import ForgotPasswordBox from "../components/ForgotPasswordBox";
+
+
+
+function ResetPasswordPage() {
+    const { user, setUser } = useContext(UserContext);
+
+    const initialUsers = [ // This is your initial list of users
+        { id: 1, name: 'Steven Bagatini' },
+        { id: 2, name: 'Natalie Smith' },
+        { id: 3, name: 'John Doe' },
+        // Add more user data
+    ];
+
+    return (
+        <>
+            <ForgotPasswordBox />
+        </>
+    );
+}
+
+export default ResetPasswordPage;
